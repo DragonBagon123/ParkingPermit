af423f0ee85935132330c5c69ad882722275bb2d
DragonBagon123 <154392888+DragonBagon123@users.noreply.github.com>
2026-03-23 01:25:36 -0500

    Update CarpoolDiscount.java

--- parking_permit/src/parking_permit/CarpoolDiscount.java
+++ parking_permit/src/parking_permit/CarpoolDiscount.java
@@ -1,7 +1,7 @@
 package parking_permit;
 
 import java.math.BigDecimal;
-///discount of 10%
+
 public class CarpoolDiscount implements RateModifer {
     @Override
     public BigDecimal apply(BigDecimal currentMonthly) {
